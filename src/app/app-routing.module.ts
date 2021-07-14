5b2de79d8008a889135126f2629c27ed73cefcf7
Parth Agarwal <agarwalparth777@gmail.com>
2021-07-14 16:49:53 -0700

    File Structure Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SsoComponent } from "./pages/sso/sso.component";
+import { EngineComponent } from "./pages/engine/engine.component";
+
+const routes: Routes = [
+  { path: '', component: EngineComponent },
+  { path: 'sso', component: SsoComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AddTaskComponent } from './core/add-task/add-task.component';
+import { TaskListComponent } from './core/task-list/task-list.component';
+import { SsoComponent } from './pages/sso/sso.component';
+import { EngineComponent } from './pages/engine/engine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddTaskComponent,
+    TaskListComponent,
+    SsoComponent,
+    EngineComponent,
   ],
   imports: [
     BrowserModule,
